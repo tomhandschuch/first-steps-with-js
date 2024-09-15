71a55c5a42676ee1d9e205492432b7bfc59948a7
Tom Handschuch <tom.handschuch@gmail.com>
2024-09-15 23:11:42 +0200

    mastered conditional blocks

---
+++ if-and-switch-statements/script.js
@@ -0,0 +1,59 @@
+const size = 25;
+let result;
+
+if (size === 10) {
+  result = "Its 10";
+} else if (size > 20) {
+  result = "Greater than 20";
+} else {
+  result = "Lower than 10";
+}
+
+console.log(result);
+// Returns Greater than 10 but should return Greater than 20
+
+// TODO: Implement the oddEven function
+function oddEven(number) {
+  if (number % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+}
+
+console.log(oddEven(4));
+// result should be even
+
+console.log(oddEven(3));
+// result should be odd
+
+console.log(oddEven(-1));
+// result should be odd
+
+console.log(oddEven(10));
+// result should be even
+
+// TODO: Implement the oldYoung function
+function oldYoung(age) {
+  if (age < 0) {
+    return "invalid parameter";
+  } else if (age < 10) {
+    return "children"
+  } else if (age > 60) {
+    return "elder person"
+  } else {
+    return "young person"
+  }
+}
+
+console.log(oldYoung(27));
+// result should be young person
+
+console.log(oldYoung(6));
+// result should be children
+
+console.log(oldYoung(-1));
+// result should be invalid parameter
+
+console.log(oldYoung(86));
+// result should be elder person
