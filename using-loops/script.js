73c5eea2c11afec5a62695447d90957b769295d0
Tom Handschuch <tom.handschuch@gmail.com>
2024-09-16 15:47:47 +0200

    updated loops

--- using-loops/script.js
+++ using-loops/script.js
@@ -1,31 +1,28 @@
 // TODO: Implement the oddNumbers function
-function oddNumbers() {
-  const ranges = [
-    [0, 4],
-    [10, 33],
-    [9, 11],
-  ];
 
-  for (let range of ranges) {
-    let start = range[0];
-    let end = range[1];
-
+function oddNumbers(start, end) {
+    if (start < 0 || end < 0) {
+      return "Both numbers must be positive.";
+    }
+  
+    let oddNums = [];
+  
     for (let i = start; i <= end; i++) {
       if (i % 2 !== 0) {
-        console.log(i);
+        oddNums.push(i);
       }
     }
+    return oddNums.join(",");
   }
-}
-
-//console.log(oddNumbers(0, 4));
-// result should be: 1,3
-
-//console.log(oddNumbers(10, 33));
-// result should be: 11,13,15,17,19,21,23,25,27,29,31,33
-
-console.log(oddNumbers(9, 12));
-// result should be: 9,11
+  
+  console.log(oddNumbers(0, 4));
+  // result should be: 1,3
+  
+  console.log(oddNumbers(10, 33));
+  // result should be: 11,13,15,17,19,21,23,25,27,29,31,33
+  
+  console.log(oddNumbers(9, 12));
+  // result should be: 9,11 
 
 // TODO: Implement the charCount function
 
